94f67b5e9c18666aea5af4889a554351b9ea214b
khoonseng <khoonseng87@gmail.com>
2026-04-25 15:08:42 +0800

    build simple flask with LLM and prediction

---
+++ app.py
@@ -0,0 +1,57 @@
+# app.py - flask app
+from flask import Flask, render_template, request
+from groq import Groq
+import joblib
+
+import os
+from dotenv import load_dotenv
+
+if os.path.exists('.env'):
+   load_dotenv()
+
+client = Groq()
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    name = request.form.get("q")
+    return(render_template("main.html"))
+
+@app.route("/chatbot",methods=["GET","POST"])
+def chatbot():
+    return(render_template("chatbot.html"))
+
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
+@app.route("/llama_result",methods=["GET","POST"])
+def llama_result():
+    q = request.form.get("q")
+    r = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": q}])
+    r = r.choices[0].message.content
+    return(render_template("llama_result.html",r=r))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction",methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    model = joblib.load("DBS_SGD_model.pkl")
+    r = model.predict([[q]])
+    return(render_template("dbs_prediction.html",r=r[0][0]))
+    # return(render_template("dbs_prediction.html",r=r))
+    # return(render_template("dbs_prediction.html",r=r[0]))
+
+if __name__ == "__main__":
+    app.run()
